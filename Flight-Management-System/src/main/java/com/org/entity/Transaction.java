ed96d731a07d044be326085dff8b10d46c715acd
arpitgarg39 <33248503+arpitgarg39@users.noreply.github.com>
2020-08-29 12:37:00 +0530

    Initial project

---
+++ Flight-Management-System/src/main/java/com/org/entity/Transaction.java
@@ -0,0 +1,34 @@
+package com.org.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "transaction")
+public class Transaction {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long passengerId;
+
+  private int noOfPassenger;
+  private double TotalAmount;
+  private double discount;
+  private double finalAmount;
+
+  @OneToOne
+  @JoinColumn(name = "user_id")
+  private User user;
+  @OneToOne
+  @JoinColumn(name = "id")
+  private Flight flight;
+
+}
